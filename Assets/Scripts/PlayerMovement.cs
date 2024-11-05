cd242d99ba8c9618f0cfcd456f7b8078c87811e6
JPLDN <jujumaster2603@gmail.com>
2024-11-05 10:23:07 +0000

    Movement Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,108 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Varibales: transform for object references, floats are numbers either - or +, Layermask references layers, bool for true or false, Vectors for directions and magnitudes
-    // CharacterController is a reference to the controller component
-    [SerializeField] Transform playerCamera;
-    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
-    [SerializeField] bool cursorLock = true;
-    [SerializeField] float mouseSensitivity = 3.5f;
-    [SerializeField] float speed = 6.0f;
-    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
-    [SerializeField] float gravity = -30f;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask ground;
+    public Camera playerCamera;
+    public float walkSpeed = 3f;
+    public float runSpeed = 7f;
+    public float jumpPower = 7f;
+    public float gravity = 12f;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    public float defaultHeight = 2f;
+    public float crouchHeight = 1f;
+    public float crouchSpeed = 3f;
 
-    public float jumpHeight = 6f;
-    float velocityY;
-    bool isGrounded;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private CharacterController characterController;
 
-    float cameraCap;
-    Vector2 currentMouseDelta;
-    Vector2 currentMouseDeltaVelocity;
+    private bool canMove = true;
 
-    CharacterController controller;
-    Vector2 currentDir;
-    Vector2 currentDirVelocity;
-    Vector3 velocity;
-
-    // Start is called before the first frame update
     void Start()
     {
-        // hitting play defines our varibale by using GetComponent to tell script about CharacterController.
-        // if state is bool = true, lock and show cursor
-        controller = GetComponent<CharacterController>();
-
-        if (cursorLock)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = true;
-        }
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        UpdateMouse();
-        UpdateMove();
-    }
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
 
-    void UpdateMouse()
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        // new Vector 2 for x and y of mouse movement, damping the movement between the target and current position.
-        // Defining CameraCap Varibale and setting limits in the y axis using Mathf Clamp, specifing that we're talking about angles.
-        // *cam movement by mouse sense and Vectors adding force.
-    {
-        Vector2 targetMouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxis("Mouse Y"));
-
-        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
-
-        cameraCap -= currentMouseDelta.y * mouseSensitivity;
-
-        cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
-
-        playerCamera.localEulerAngles = Vector3.right * cameraCap;
-
-        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
-    }
-
-    void UpdateMove()
-    {
-
-        // Defines true condition for isGrounded and also Dimensions. Using a vectr for a target direction when using WASD, instant force.
-        // Normalized to stop doubling speed when holding W & D or W & A. Smoothing force by damping between instant push of target and velocity taking in value of smooth move.
-        // Defining VelocityY which is added and equal to gravity multiplied by 2f and Time.deltaTime. 
-        // Defining another Vector3 pushing forward * current y and doing same with x axis * speed float. Vector up for upwards move * velocityY
-        // Moving controller with variable which defines movement and *Time.deltaTime.
-        // if statements conditions are met when it does calculation of all 3 values * and then square rooted which defines the value of velocityY which we added earlier.
-        // Final thing fixes a bug with script
-
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
-
-        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontol"), Input.GetAxis("Vertical"));
-        targetDir.Normalize();
-
-        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
-
-        velocityY += gravity * 2f * Time.deltaTime;
-
-        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * speed + Vector3.up * velocityY;
-
-        controller.Move(velocity * Time.deltaTime);
-
-        if (isGrounded && Input.GetButtonDown("Jump")) ;
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            moveDirection.y = jumpPower;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
         }
 
-        if (isGrounded! && controller.velocity.y < -1f)
+        if (!characterController.isGrounded)
         {
-            velocityY = -8f;
+            moveDirection.y -= gravity * Time.deltaTime;
         }
 
+        if (Input.GetKey(KeyCode.R) && canMove)
+        {
+            characterController.height = crouchHeight;
+            walkSpeed = crouchSpeed;
+            runSpeed = crouchSpeed;
+
+        }
+        else
+        {
+            characterController.height = defaultHeight;
+            walkSpeed = 6f;
+            runSpeed = 12f;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
-
-
